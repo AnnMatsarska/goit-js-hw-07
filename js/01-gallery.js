a5275c48b303e47809967651352bace6865174b9
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-07-24 17:35:36 +0300

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+container.addEventListener('click', onClick);
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+container.insertAdjacentHTML('beforeend', markup);
+
+function onClick(evt) {
+  evt.preventDefault();
+  const instance = basicLightbox.create(`
+   <img src="${evt.target.dataset.source}" width="800" height="600"> `);
+
+  instance.show();
+
+  container.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
